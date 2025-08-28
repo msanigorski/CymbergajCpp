33603316ba15265c989097a48b444cca36d30e16
msanigorski <mateusz.sanigorski@inlader.pl>
2025-08-28 10:10:34 +0100

    Update

---
+++ build/CMakeFiles/robot_test_http.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robot_test_http.dir/src/capture.cpp.o"
+  "CMakeFiles/robot_test_http.dir/src/capture.cpp.o.d"
+  "CMakeFiles/robot_test_http.dir/src/detector.cpp.o"
+  "CMakeFiles/robot_test_http.dir/src/detector.cpp.o.d"
+  "CMakeFiles/robot_test_http.dir/src/robot_test_http.cpp.o"
+  "CMakeFiles/robot_test_http.dir/src/robot_test_http.cpp.o.d"
+  "CMakeFiles/robot_test_http.dir/src/tracker.cpp.o"
+  "CMakeFiles/robot_test_http.dir/src/tracker.cpp.o.d"
+  "CMakeFiles/robot_test_http.dir/src/utils.cpp.o"
+  "CMakeFiles/robot_test_http.dir/src/utils.cpp.o.d"
+  "robot_test_http"
+  "robot_test_http.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robot_test_http.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/robot_test_http.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for robot_test_http.
+# This may be replaced when dependencies are built.
